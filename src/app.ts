670678bcd1460736b69fea7d560db49c85ba5916
IgorHamzi <igorwhamzi@gmail.com>
2022-09-28 17:57:10 -0300

    feat: add middlewares generic

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
 
 const app = express();
+app.use(express.json());
+app.use(errorHandler);
 
 export default app;
+
+// ErrorHandler inspirado na aula ao vivo 12.1(antiga 30.1)
