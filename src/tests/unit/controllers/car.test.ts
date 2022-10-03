751e931539bfd637cbb1060fc76b1fec9d56dbd3
IgorHamzi <igorwhamzi@gmail.com>
2022-10-03 09:07:08 -0300

    feat: add test car controller 'create'

---
+++ src/tests/unit/controllers/car.test.ts
@@ -0,0 +1,38 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import CarModel from '../../../models/Car'
+import CarService from '../../../services/CarService'
+import CarController from '../../../controllers/CarController';
+import { Request, Response } from 'express';
+import { carMock } from '../../mocks/carMock';
+
+describe('Car controller', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
+  const carController = new CarController(carService);
+
+  const req = {} as Request;
+  const res = {} as Response;
+
+  before(() => {
+    sinon.stub(carService, 'create').resolves(carMock);
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  describe('Create Car', () => {
+    it('sucess', async () => {
+      req.body = carMock;
+      await carController.createCar(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    })
+  });
+});
