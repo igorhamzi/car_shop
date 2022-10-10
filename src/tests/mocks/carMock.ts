3192b14d9aa3460fb0656db972c1bbfc427870c4
IgorHamzi <igorwhamzi@gmail.com>
2022-10-10 14:11:04 -0300

    feat: add mocks for update

--- src/tests/mocks/carMock.ts
+++ src/tests/mocks/carMock.ts
@@ -19,7 +19,28 @@ const carMockWithId:ICar & { _id:string } = {
   doorsQty: 2
 };
 
+const carMockForUpdate:ICar = {
+  model: "Fiat Uno",
+  year: 1963,
+  color: "blue",
+  buyValue: 3500,
+  seatsQty: 4,
+  doorsQty: 4
+};
+
+const carMockForUpdateWithId:ICar & { _id:string } = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Fiat Uno",
+  year: 1963,
+  color: "blue",
+  buyValue: 3500,
+  seatsQty: 4,
+  doorsQty: 4
+};
+
 export {
   carMock,
   carMockWithId,
+  carMockForUpdate,
+  carMockForUpdateWithId
 };
