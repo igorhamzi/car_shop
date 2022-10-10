dd9674fbed38d5226090cd03759c5fc0a3b59081
IgorHamzi <igorwhamzi@gmail.com>
2022-10-10 16:12:15 -0300

    feat: create IMotorcycle interface

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { vehicleZodSchema } from './IVehicle';
+
+export const motorcycleZodSchema = vehicleZodSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number().int().min(1).max(2500),
+});
+
+export type IMotorcycle = z.infer<typeof motorcycleZodSchema>;
+
+export default IMotorcycle;
