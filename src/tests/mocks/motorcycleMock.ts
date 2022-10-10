68e787bef1cb1563290292beece6c0d82077d2d1
IgorHamzi <igorwhamzi@gmail.com>
2022-10-10 17:17:12 -0300

    test: add motorcycle mocks

---
+++ src/tests/mocks/motorcycleMock.ts
@@ -0,0 +1,48 @@
+import IMotorcycle from "../../interfaces/IMotorcycle";
+
+const motorcycleMock:IMotorcycle = {
+  model: "Honda CG Titan 125",
+  year: 1963,
+  color: "red",
+  buyValue: 3500,
+  category: "Street",
+  engineCapacity: 125
+
+};
+
+const motorcycleMockWithId:IMotorcycle & { _id:string } = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Honda CG Titan 125",
+  year: 1963,
+  color: "red",
+  buyValue: 3500,
+  category: "Street",
+  engineCapacity: 125
+
+};
+
+const motorcycleMockForUpdate:IMotorcycle = {
+  model: "Honda CG Titan 125",
+  year: 1963,
+  color: "black",
+  buyValue: 3500,
+  category: "Street",
+  engineCapacity: 125
+};
+
+const motorcycleMockForUpdateWithId:IMotorcycle & { _id:string } = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Honda CG Titan 125",
+  year: 1963,
+  color: "black",
+  buyValue: 3500,
+  category: "Street",
+  engineCapacity: 125
+};
+
+export {
+  motorcycleMock,
+  motorcycleMockWithId,
+  motorcycleMockForUpdate,
+  motorcycleMockForUpdateWithId
+};
